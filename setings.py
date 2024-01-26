86aac04b6bb6a2cd71ab295021b2ba913c78dac6
franch.ecped23_trian <pedrofe.av@gmail.com>
2024-01-26 11:00:10 +0100

    cambiando mapa

--- setings.py
+++ setings.py
@@ -1,7 +1,13 @@
+from io import open
+
 WIDTH = 1280
 HEIGTH = 720
 FSP = 60
 TILESIZE = 64
+
+WORLD_MAP2 = open('map/map.txt')
+
+
 
 WORLD_MAP = [
 ['x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x'],
