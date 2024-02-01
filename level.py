e154e9087393761a2c9be8427b091ce458ffd3aa
Pedro Franch Echániz <pedrofe.av@gmail.com>
2024-02-01 21:27:50 +0100

    implementación ganar

--- level.py
+++ level.py
@@ -5,22 +5,27 @@ from tile import Tile
 from player_prove import *
 from player_prove import Player
 from potion import Potion
+from coin import Coin
 
 class Level:
     def __init__(self):
 
         self.display_surface = pygame.display.get_surface()
 
-
         self.visible_sprites = YSortCameraGroup()
         self.obstacles_sprites = pygame.sprite.Group()
+        self.collectible_sprites = pygame.sprite.Group()
 
         self.map_data, self.object_counts = self.read_map_and_objects('map/map.txt')
+
+        self.total_coins = sum(value for key, value in self.object_counts.items() if key == 'd')
 
         self.free_spaces = []
 
         self.create_map()
         self.place_objects()
+        self.player_won = False
+
 
     def read_map_and_objects(self, filename):
         with open(filename, 'r') as file:
@@ -53,7 +58,9 @@ class Level:
                 if col == 'w':
                     Tile((x,y), [self.visible_sprites, self.obstacles_sprites], 'magma')
                 if col == 'p':
-                    self.player = Player((x, y), [self.visible_sprites], self.obstacles_sprites)
+                    self.player = Player((x, y), [self.visible_sprites], self.obstacles_sprites,
+                                         self.collectible_sprites, self)
+                    self.player.coins = 0
                 if col == ' ':
                     self.free_spaces.append((x, y))
 
@@ -64,12 +71,44 @@ class Level:
                     pos = random.choice(self.free_spaces)
                     self.free_spaces.remove(pos)
                     if obj_type == 'o':
-                        Potion(pos, [self.visible_sprites])
+                        Potion(pos, [self.visible_sprites, self.collectible_sprites])
+                    elif obj_type == 'd':
+                        Coin(pos, [self.visible_sprites, self.collectible_sprites])
 
+    def restart_level(self):
+        self.visible_sprites.empty()
+        self.obstacles_sprites.empty()
+        self.collectible_sprites.empty()
+        self.free_spaces.clear()
+        self.create_map()
+        self.place_objects()
+        self.player.coins = 0
+        self.player.health = 10
+        self.player.rect.topleft = self.player.starting_pos
+        self.player_won = False
 
     def run(self):
         self.visible_sprites.curstom_draw(self.player)
         self.visible_sprites.update()
+        self.check_win()
+        if self.player_won:
+            self.display_victory_message()
+    def check_win(self):
+        if self.player.coins == self.total_coins:
+            self.player_won = True
+
+    def display_victory_message(self):
+        font = pygame.font.Font(None, 50)  # Puedes usar una ruta de archivo si tienes una fuente específica
+
+        text_surface = font.render("¡Has ganado!", True, (255, 255, 255))  # Texto blanco
+
+        screen_width, screen_height = self.display_surface.get_size()
+        text_width, text_height = text_surface.get_size()
+
+        x = (screen_width - text_width) // 2
+        y = (screen_height - text_height) // 2
+
+        self.display_surface.blit(text_surface, (x, y))
 
 class YSortCameraGroup(pygame.sprite.Group):
     def __init__(self):
--- player_prove.py
+++ player_prove.py
@@ -1,27 +1,31 @@
 import pygame
 from setings import *
+from coin import Coin
+from potion import Potion
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, groups, obstacles_sprites):
+    def __init__(self, pos, groups, obstacles_sprites, collectible_sprites, level):
         super().__init__(groups)
         self.image = pygame.image.load('assets/character.png').convert_alpha()
-        self.rect = self.image.get_rect(topleft = pos)
+        self.rect = self.image.get_rect(topleft=pos)
         self.health = 10
         self.direction = pygame.math.Vector2()
         self.speed = 5
         self.last_magma = None
         self.obstacles_sprites = obstacles_sprites
+        self.collectible_sprites = collectible_sprites
+        self.level = level
+        self.coins = 0
+        self.starting_pos = pos
 
     def input(self):
         keys = pygame.key.get_pressed()
-
         self.direction.x = keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]
         self.direction.y = keys[pygame.K_DOWN] - keys[pygame.K_UP]
 
     def move(self, speed):
         if self.direction.magnitude() != 0:
             self.direction = self.direction.normalize() * speed
-
         self.rect.x += self.direction.x
         self.collision('horizontal')
         self.rect.y += self.direction.y
@@ -46,6 +50,10 @@ class Player(pygame.sprite.Sprite):
                         elif self.direction.y < 0:
                             self.rect.top = sprite.rect.bottom
 
+    def check_death(self):
+        if self.health <= 0:
+            self.restart_game()
+
     def check_magma_collision(self):
         current_magma = None
         for sprite in self.obstacles_sprites:
@@ -61,7 +69,41 @@ class Player(pygame.sprite.Sprite):
 
         self.last_magma = current_magma
 
+    def check_potion_collision(self):
+        for potion in self.get_colliding_sprites(Potion, self.collectible_sprites):
+            self.health += 2
+            potion.kill()
+
+    def check_coin_collision(self):
+        for coin in self.get_colliding_sprites(Coin, self.collectible_sprites):
+            self.coins += 1
+            coin.kill()
+
+    def get_colliding_sprites(self, sprite_type, group):
+        return [sprite for sprite in group if isinstance(sprite, sprite_type) and self.rect.colliderect(sprite.rect)]
+
     def update(self):
         self.input()
         self.move(self.speed)
         self.check_magma_collision()
+        self.check_potion_collision()
+        self.check_coin_collision()
+        self.check_death()
+
+    def check_death(self):
+        if self.health <= 0:
+            self.restart_game()
+
+    def restart_game(self):
+        print("Has perdido. El juego se reiniciará.")
+        self.health = 10
+        self.coins = 0
+        self.rect.topleft = self.starting_pos
+        self.level.restart_level()
+
+    def update(self):
+        self.input()
+        self.move(self.speed)
+        self.check_magma_collision()
+        self.check_potion_collision()
+        self.check_coin_collision()
